84d2e73766d0da5aedc6f8f98f137b70325fd930
Merab Samkharadze <samkharadzemerab@gmail.com>
2023-12-23 16:52:15 +0400

    add author input

---
+++ new-blog/script.js
@@ -0,0 +1,39 @@
+const author = getElement("author-input");
+const header = getElement("header-input");
+
+author.addEventListener("keyup", (e) => {
+  const filters = e.target.nextElementSibling.children;
+
+  if (e.target.value.trim().split(" ").join("").length < 4) {
+    filters[0].style.color = "#EA1919";
+    author.style.background = "#FAF2F3";
+    author.style.border = "1px solid #EA1919";
+  } else if (e.target.value.trim().split(" ").join("").length > 3) {
+    filters[0].style.color = "#14D81C";
+  }
+
+  if (e.target.value.trim().split(" ").length < 2) {
+    filters[1].style.color = "#EA1919";
+    author.style.background = "#FAF2F3";
+    author.style.border = "1px solid #EA1919";
+  } else if (e.target.value.trim().split(" ").length > 1) {
+    filters[1].style.color = "#14D81C";
+  }
+
+  if (!/^[ა-ჰ ]+$/.test(e.target.value)) {
+    filters[2].style.color = "#EA1919";
+    author.style.background = "#FAF2F3";
+    author.style.border = "1px solid #EA1919";
+  } else if (/^[ა-ჰ ]+$/.test(e.target.value)) {
+    filters[2].style.color = "#14D81C";
+  }
+
+  if (
+    e.target.value.trim().split(" ").join("").length > 3 &&
+    /^[ა-ჰ ]+$/.test(e.target.value) &&
+    e.target.value.trim().split(" ").length > 1
+  ) {
+    author.style.background = "#F8FFF8";
+    author.style.border = "1px solid #14D81C";
+  }
+});
