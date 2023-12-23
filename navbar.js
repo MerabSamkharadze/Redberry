df1788ae38934e2644a5a982191b56b5ef26f248
Merab Samkharadze <samkharadzemerab@gmail.com>
2023-12-23 15:16:33 +0400

    finish login logic

--- navbar.js
+++ navbar.js
@@ -3,23 +3,34 @@ const getElement = (elementId) => document.getElementById(elementId);
 const loginButton = getElement("login-button");
 const overlay = getElement("overlay");
 const loginForm = getElement("login-form");
+const successButton = getElement("success-button");
 const loginInput = getElement("login-input");
 const formButton = getElement("form-button");
 const closeIcon = getElement("close-icon");
+const loginModal = getElement("login-modal");
+const successModal = getElement("success-modal");
+const blogAddButton = getElement("blog-add-button");
 const error = getElement("error");
 
 const loginUser = async (value) => {
   const res = await fetch("https://api.blog.redberryinternship.ge/api/login", {
     method: "POST",
-    body: JSON.stringify({ email: value }),
+    body: JSON.stringify({
+      email: value,
+    }),
     headers: {
       accept: "application/json",
       "Content-Type": "application/json",
     },
   });
 
-  const data = await res.json();
-  if (data.message === "The email field must be a valid email address.") {
+  if (res.status === 204) {
+    loginModal.classList.add("invincible");
+    successModal.classList.remove("invincible");
+    localStorage.setItem("login", true);
+    blogAddButton.classList.remove("invincible");
+    loginButton.classList.add("invincible");
+  } else {
     error.classList.remove("invincible");
     error.lastElementChild.innerText = "ელ-ფოსტა არ მოიძებნა";
   }
@@ -28,6 +39,11 @@ const loginUser = async (value) => {
 loginButton.addEventListener("click", () => {
   overlay.classList.remove("invincible");
   loginForm.classList.remove("invincible");
+});
+
+successButton.addEventListener("click", () => {
+  overlay.classList.add("invincible");
+  loginForm.classList.add("invincible");
 });
 
 overlay.addEventListener("click", () => {
@@ -45,6 +61,8 @@ formButton.addEventListener("click", () => {
     error.classList.remove("invincible");
     error.lastElementChild.innerText = "ველი სავალდებულოა";
     return;
+  } else if (!loginInput.value.endsWith("@redberry.ge")) {
+    return;
   }
 
   loginUser(loginInput.value);
@@ -59,3 +77,13 @@ loginInput.addEventListener("keyup", (e) => {
       "მეილი უნდა მთავრდებოდეს @redberry.ge-ით";
   }
 });
+
+const loggedIn = JSON.parse(localStorage.getItem("login"));
+
+if (loggedIn) {
+  blogAddButton.classList.remove("invincible");
+  loginButton.classList.add("invincible");
+} else {
+  blogAddButton.classList.add("invincible");
+  loginButton.classList.remove("invincible");
+}
